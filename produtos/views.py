eeefd1b3df92ce6d9c8371e3bc5d3a2d8e010c03
Kauan <kauanmonteirosilva789@gmail.com>
2024-12-12 10:35:07 -0300

    Melhorias na interface

--- produtos/views.py
+++ produtos/views.py
@@ -5,3 +5,7 @@ def home(request):
     produto = Produto.objects.all()
     categoria = Categoria.objects.all()
     return render(request,'pages/home.html',{'produto':produto,'categoria':categoria})
+
+def vermaisproduto(request, id):
+    produto = Produto.objects.get(pk=id)
+    return render(request,'pages/vermaisproduto.html',{'produto':produto})
